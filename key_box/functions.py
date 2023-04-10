22152868b0b14af2db91de4d9d1057252d04fcfa
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-04-10 13:04:41 +0100

    add comment to functions

--- key_box/functions.py
+++ key_box/functions.py
@@ -3,6 +3,10 @@ FILEPATH = "keys.csv"
 
 
 def get_key_in_dict():
+    """ This function read the CSV file and
+    return a dictionary that contain shop ID
+    as keys and keybox as values
+    """
     with open(FILEPATH, "r") as file:
         content = csv.reader(file)
         datas = dict(content)
@@ -10,6 +14,9 @@ def get_key_in_dict():
 
 
 def get_key_in_list():
+    """ This function read the CSV file and
+    return a list of lists
+    """
     with open(FILEPATH, "r") as file:
         content = csv.reader(file)
         datas = list(content)
@@ -17,18 +24,27 @@ def get_key_in_list():
 
 
 def append_new_key(outlet_to_add):
+    """ This function appends new list into
+    a new line in the filepath
+    """
     with open(FILEPATH, "a", newline='') as file:
         data = csv.writer(file)
         data.writerow(outlet_to_add)
 
 
 def new_file_content(content):
+    """ This function write updated data back
+    into the filepath.
+    """
     with open(FILEPATH, mode="w", newline="") as file:
         data = csv.writer(file)
         data.writerows(content)
 
 
 def delete_key(content, outlet_to_delete):
+    """ This function take 2 arguments and delete second
+    argument from a list (the first argument)
+    """
     for value in content:
         if value[0] == outlet_to_delete:
             content.remove(value)
